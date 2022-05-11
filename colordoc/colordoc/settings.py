f5d8cb2ae7165d92fad9719a4a398d236e524581
cromtech1979 <ankostov@gmail.com>
2022-05-11 18:20:29 +0300

    UI

--- colordoc/colordoc/settings.py
+++ colordoc/colordoc/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
